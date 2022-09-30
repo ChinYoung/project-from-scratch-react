3bb68477a4d92f9112a339ab9a565c9f4388e593
ChinYoung <woodichin@outlook.com>
2022-09-30 14:20:03 +0800

    remove duplicated settings

--- vite.config.js
+++ vite.config.js
@@ -1,22 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react({
-    babel: {
-      plugins: ['@babel/plugin-transform-react-jsx', '@babel/plugin-syntax-jsx'],
-    },
-  })],
-  base: '/libra',
+  base: "/libra",
   server: {
     proxy: {
-      '/api': {
-        target: 'https://www.rakki.fun:30789',
+      "/api": {
+        target: "https://www.rakki.fun:30789",
         secure: true,
         changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ''),
+        rewrite: (path) => path.replace(/^\/api/, ""),
       },
     },
   },
-})
+});
